3ff251b30a039a8b12e3623e6a15602de5745923
Yusuf Aktepe <yusuf@yusufaktepe.com>
2023-05-30 18:17:43 +0300

    nvim: replace `nvim-colorizer` with `uga-rosa/ccc.nvim`

--- .config/nvim/lua/user/plugins/misc.lua
+++ .config/nvim/lua/user/plugins/misc.lua
@@ -41,28 +41,15 @@ return {
 
   -- colorizer
   {
-    "NvChad/nvim-colorizer.lua",
-    cmd = "ColorizerToggle",
+    "uga-rosa/ccc.nvim",
+    cmd = { "CccHighlighterToggle", "CccHighlighterEnable", "CccPick", "CccConvert" },
     ft = { "css", "javascript" },
     keys = {
-      { "<leader>uh", "<cmd>ColorizerToggle<cr>", desc = "Toggle color [h]ighlight" },
+      { "<leader>uh", "<cmd>CccHighlighterToggle<cr>", desc = "Toggle color [h]ighlight" },
     },
     opts = {
-      filetypes = { "*", "!lazy" },
-      buftype = { "*", "!prompt", "!nofile" },
-      user_default_options = {
-        RGB = true, -- #RGB hex codes
-        RRGGBB = true, -- #RRGGBB hex codes
-        names = false, -- "Name" codes like Blue
-        RRGGBBAA = true, -- #RRGGBBAA hex codes
-        AARRGGBB = false, -- 0xAARRGGBB hex codes
-        rgb_fn = true, -- CSS rgb() and rgba() functions
-        hsl_fn = true, -- CSS hsl() and hsla() functions
-        css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-        -- Available modes for `mode`: foreground, background,  virtualtext
-        mode = "background", -- Set the display mode.
-        virtualtext = "■",
+      highlighter = {
+        auto_enable = true,
       },
     },
   },
